ee0ad01baaedfe7884270d306f7c182b25685ce2
João Pedro Pimenta <125268689+Pimenta15@users.noreply.github.com>
2023-09-22 10:58:44 -0300

    Update em LSEint.java

--- Listas/Listainteira/inteira/LSEint.java
+++ Listas/Listainteira/inteira/LSEint.java
@@ -6,39 +6,68 @@ public class LSEint {
 
     public void inserirNoInicio(Integer num){
 
-        LSEnode novo;
+        LSEnode novo, retorno;
         
         novo = new LSEnode(num);
-       
-        if (this.primeiro == null) {
+       if(isEmpty()){
+           this.primeiro = num;
+       }else{
+           retorno = procurar(valor);
+           
+        if (retorno == null) {
             this.primeiro = novo;
         }
         else{
             novo.setProx(this.primeiro);
             this.primeiro = novo;
         }
+        }
     }
-
     public void removerPrimeiro(){
-
+        if(!isEmpty()){
         this.primeiro = primeiro.getProx();
-
+            System.out.println("Remoção efetuada");
+        }else{
+            System.out.println("Lista Vazia");
+        }
     }
-
-    public int procurar(Integer buscado){
+    
+    public boolean isEmpty () {
+    
+        return this.primeiro == null;
+    }
+    
+    public int buscar(Integer buscado){
        
-        int contador = 1;
-        LSEnode atual = primeiro;
-
+        int contador = 0;
+        LSEnode atual = this.primeiro;
+        if(!isEmpty()){
         while (atual != null) {
-            if (atual.getInfo().equals(buscado)) {
-                return contador;
+            
+            if (atual.getInfo() == buscado) {
+                 contador++;
             }
             atual = atual.getProx();
-            contador++;
+            return contador;
         }
-
+        }
     return -1;
 
+    }
+    public LSENode procurar(int valor){
+       
+        LSEnode atual = this.primeiro;
+        
+        if(!isEmpty()){
+            
+        while (atual != null) {
+            
+            if (atual.getInfo() == valor) {
+                return atual;
+            }
+            atual = atual.getProx();
+        }
+        }
+    return null;
     }
 }
