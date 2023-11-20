ac76e83af21dd155ba389dbb86c4ed688ce381b7
João Pedro Pimenta <125268689+Pimenta15@users.noreply.github.com>
2023-11-20 19:21:43 -0300

    locadora

--- App.java
+++ App.java
@@ -1,34 +1,82 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner in =  new Scanner(System.in);
-        LDEInt numeros = new LDEInt();
-        int op, num ,total;
-        do{
-           
-            op = in.nextInt();
-            switch (op) {
+
+    private static ListaCategorias lista = new ListaCategorias();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int opcao;
+
+        do {
+            exibirMenu();
+            System.out.print("Escolha uma opção: ");
+            opcao = in.nextInt();
+            in.nextLine(); 
+
+            switch (opcao) {
                 case 1:
-                    System.out.println("informe o valor");
-                    num = in.nextInt();
-                    numeros.inserir(num);
+                    cadastrarFilme();
                     break;
                 case 2:
-                    numeros.exibirTodos();
+                    removerFilme();
                     break;
                 case 3:
-                    System.out.println("Informe o valor");
-                    num = in.nextInt();
-                    total = numeros.remover(num);
-                    System.out.println("totam: "+ total);
+                    consultarFilme();
                     break;
-            
+                case 4:
+                    editarFilme();
+                    break;
+                case 5:
+                    exibirTodosFilmes();
+                    break;
+                case 6:
+                    exibirFilmesPorCategoria();
+                    break;
+                case 0:
+                    System.out.println("Saindo da aplicação. Até mais!");
+                    break;
                 default:
-                    break;
+                    System.out.println("Opção inválida. Tente novamente.");
             }
-        }while(op!=0);
 
+        } while (opcao != 0);
+
+        
     }
-    
+
+    private static void exibirMenu() {
+        System.out.println("\n=== Menu ===");
+        System.out.println("1. Cadastrar Filme");
+        System.out.println("2. Remover Filme");
+        System.out.println("3. Consultar Filme");
+        System.out.println("4. Editar Filme");
+        System.out.println("5. Exibir Todos os Filmes");
+        System.out.println("6. Exibir Filmes por Categoria");
+        System.out.println("0. Sair");
+    }
+
+    private static void cadastrarFilme() {
+        
+    }
+
+    private static void removerFilme() {
+      
+    }
+
+    private static void consultarFilme() {
+      
+    }
+
+    private static void editarFilme() {
+       
+    }
+
+    private static void exibirTodosFilmes() {
+       
+    }
+
+    private static void exibirFilmesPorCategoria() {
+      
+    }
 }
